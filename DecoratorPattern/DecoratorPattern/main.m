b24b2f9b09779f38055548b79cd150c4cbbfe6e0
Junwei Ge <gejunwei110@gmail.com>
2016-08-30 17:46:45 +0800

    features: create DecoratorPattern project
    Signed-off-by: Junwei Ge <gejunwei110@gmail.com>

---
+++ DecoratorPattern/DecoratorPattern/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DecoratorPattern
+//
+//  Created by Utopia on 16/8/30.
+//  Copyright © 2016年 Utopia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
